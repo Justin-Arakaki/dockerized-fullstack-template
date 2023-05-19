df06b001399b9e3a0a77bf68124fc20d735933a6
Justin-Arakaki <Justiniankoi@gmail.com>
2023-05-19 14:29:53 -0700

    simplified Errors and removed errorHandlers

--- backend/src/api/errors/ForbiddenError.ts
+++ backend/src/api/errors/ForbiddenError.ts
@@ -1,17 +1,6 @@
-type ForbiddenErrorType =
-  | 'Access Denied'
-  | 'Unauthorized User'
-  | 'Insufficient Privileges'
-  | 'RBAC Violation'
-  | 'Resource Ownership Violation'
-  | 'Expired or Revoked Access Token';
-
 export default class UnauthorizedError extends Error {
-  public errorType: ForbiddenErrorType;
-
-  constructor(errorType: ForbiddenErrorType, message: string = 'Forbidden') {
+  constructor(message: string = 'Error 403 Forbidden') {
     super(message);
     this.name = 'ForbiddenError';
-    this.errorType = errorType;
   }
 }
